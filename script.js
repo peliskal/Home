42bb05ab330679654c5af73c18215c792bd23461
Dagda <109911166+peliskal@users.noreply.github.com>
2023-12-19 15:24:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,179 @@
+const m3uData = `#EXTM3U 
+#EXTINF:-1,tvg-logo="logos-canales/1.png", cartoon network 1
+https://playout.cdn.cartoonnetwork.com.br/playout_06/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/1.png", cartoon network 2
+https://playout.cdn.cartoonnetwork.com.br/playout_02/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/1.png", cartoon network kids
+https://playout.cdn.cartoonnetwork.com.br/playout_04/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/4.png",cartoonito
+https://playout.cdn.cartoonnetwork.com.br/playout_08/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/5.png", canal 7 jujuy
+https://stream.arcast.live/canal7jujuy/ngrp:canal7jujuy_all/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/6.png", canal 7 catamarca
+https://stream.arcast.com.ar/canal7catamarca/ngrp:canal7catamarca_all/playlist.m3u8?DVR=
+#EXTINF:-1, tvg-logo="logos-canales/0.png", catamarca
+https://stream.arcast.com.ar/envivo/castv/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/7.png", canal 8
+https://stream.arcast.com.ar/c8mdp/ngrp:c8mdp_all/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/9.png", TVP
+https://inliveserver.com:1936/17012/17012/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/10.png", CN51 
+https://tvcn51.com/hls/cn51480.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/11.png", 14 TV
+https://tv.livestreaminggroup.info:3052/live/catorcetvlive.m3u8
+#EXTINF:-1,tvg-logo="logos-canales/12.png", Canal E
+https://unlimited1-us.dps.live/perfiltv/perfiltv.smil/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/00.png", nubeh TV
+https://vcp.myplaytv.com/nubehtv/nubehtv/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/13.png", Rural
+https://streamlovcd1.alsolnet.com/elrural/live/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/14.png", canal coco TV
+https://cloudflare.streamgato.us:3253/live/canalcocotvlive.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/15.png", Cosmos TV
+https://tv.mediacp.eu:19360/cosmos/cosmos.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/16.png", Dance FM
+https://5eaccbab48461.streamlock.net:1936/dancefm_1/dancefm_1/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/0.png", Dattalive
+https://panel.dattalive.com/8066/8066/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/17.png", Galicia
+https://america-crtvg.flumotion.com/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/18.png", RDS3
+https://rds3.desdeparaguay.net/gotv/gotv/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/19.png", La-X
+https://stream.eleden.com/livelax/ngrp:livelax_all/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/20.png", Latina TV
+https://ss3.domint.net:3156/ltv_str/latinatvint/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/21.png", Play
+https://vivo.solumedia.com:19360/playtv/playtv.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/22.png", Tierra Fertil
+https://5fc584f3f19c9.streamlock.net/agrotendencia/videoagrotendencia_hls1/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/23.png", ADN
+https://jireh-1-hls-video-ar-isp.dps.live/hls-video/931b584451fa6dd1313ee66efbfd5802e3f3bcea/adntv/adntv.smil/playlist.m3u8?dpssid=05f031a62c4c684b8502dfb8e217bbe6&sid=ba5t1l1xb41114139504656176013a193&ndvc=0
+#EXTINF:-1, tvg-logo="logos-canales/24.png", C21 TV
+https://iptv.ixfo.com.ar:30443/c21tv/hd/c21tv/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/25.png", Pandamax
+https://stream.wifiexpert.cl/telenorte/pandamaxch/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/26.png", Platzi
+https://us-b4-p-e-cg11.cdn.mdstrm.com/live-stream/647101bf042584086ee61125/playout/master.m3u8?aid=512e13acaca1ebcd2f000279&pid=wAuV9hLWmDvnxNtFLRNTx8MB5t6gEDr1&sid=0uFXFeDPe35DNfMqV9mk2ZNwxw7PewQv&uid=9MLiGOv1K6paCAIZ44P2nP6osoOvUnu8&es=us-b4-p-e-cg11.cdn.mdstrm.com&ote=1700973115065&ot=MVAUNKey6dc4jvRV4fPngA&proto=https&pz=us
+#EXTINF:-1, tvg-logo="logos-canales/27.png", INT CGTN Español
+https://livees.cgtn.com/1000e/prog_index.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/28.png", Extrema TV
+http://livestreamcdn.net:1935/ExtremaTV/ExtremaTV/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/29.png", Estrella TV
+https://estrellatv-roku.amagi.tv/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/30.png", AGUAS CALIENTE
+http://streamingcws10.com:1935/telemetrika3/videotelemetrika3/.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/31.png", Argentinisima
+https://stream1.sersat.com/hls/argentinisima.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/32.png", ATV Argentina
+https://stream.arcast.com.ar/atbargentina/atbargentina/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/33.png", Canal 13 La Rioja
+http://arcast.net:1935/mp/mp/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/34.png", Canal 21 TV
+https://iptv.ixfo.com.ar:30443/c21tv/hd/c21tv/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/35.png", 907 FM
+https://panel.dattalive.com/canal907/canal907/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/36.png", El Trece
+https://live-01-02-eltrece.vodgc.net/eltrecetv/index.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/37.png", Neo TV
+https://videostream.shockmedia.com.ar:19360/neotvdigital/neotvdigital.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/38.png", TN
+https://live-01-01-tn.vodgc.net/TN24/index.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/39.png", xtrema Anime
+https://stmv1.cnarlatam.com/xtremaanime/xtremaanime/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/40.png", Retro cartoon
+https://stmv1.cnarlatam.com/retroinfantil/retroinfantil/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/41.png", Sextavision
+https://5ff3d9babae13.streamlock.net/8020/8020/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/42.png", Telemax
+https://cdn2.streamgato.us/telemax/telemax/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/43.png", Latino Kids
+https://6362ca6f1fc28.streamlock.net/8062/8062/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/44.png", Sports Max 
+https://vcpar.myplaytv.com/esportsmax/live/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/45.png", Litus
+https://192.99.38.174:1935/litustv/ngrp:litustv_all/playlist.m3u8
+#EXTINF:-1, tvg-logo="logos-canales/46.png", Televisión Rionegrina
+https://panel.dattalive.com:1935/8204/8204/playlist.m3u8
+`;
+
+
+// Selecciona el elemento con el id 'channel-list' y lo asigna a la variable channelList
+const channelList = document.getElementById('channel-list');
+
+// Inicializa la variable hls como nula
+let hls = null;
+
+// Selecciona el elemento con el id 'player' y lo asigna a la variable player
+const player = document.getElementById('player');
+
+// Define la función playChannel que recibe una URL como parámetro
+function playChannel(url) {
+  // Verifica si hls está definido
+  if (hls) {
+    // Destruye la instancia de hls si está definida
+    hls.destroy();
+  }
+  // Crea una nueva instancia de Hls
+  hls = new Hls();
+  // Carga la fuente de video desde la URL proporcionada
+  hls.loadSource(url);
+  // Asocia la instancia de hls con el elemento player
+  hls.attachMedia(player);
+  // Escucha el evento MANIFEST_PARSED de hls y reproduce el video cuando ocurre
+  hls.on(Hls.Events.MANIFEST_PARSED, () => {
+    player.play();
+  });
+}
+
+// Divide el contenido de m3uData por saltos de línea y lo asigna a la variable lines
+const lines = m3uData.split('\n');
+
+// Itera sobre cada línea del array lines junto con su índice
+lines.forEach((line, index) => {
+  // Verifica si la línea comienza con '#EXTINF'
+  if (line.startsWith('#EXTINF')) {
+    // Obtiene el nombre del canal dividiendo la línea por comas y seleccionando el segundo elemento
+    const channelName = line.split(',')[2];
+    // Obtiene la URL del canal que se encuentra en la siguiente línea
+    const channelUrl = lines[index + 1];
+    // Obtiene la URL de la imagen que se encuentra en la siguiente línea
+    const imageUrl = line.split('"')[1];
+    // Crea un elemento de lista
+    const listItem = document.createElement('li');
+    // Establece el contenido HTML del elemento de lista con un enlace que llama a la función playChannel y muestra la imagen y el nombre del canal
+    listItem.innerHTML = `
+      <a href="#" onclick="playChannel('${channelUrl}')">
+        <img src="${imageUrl}" alt="${channelName}" width="150" height="100">
+       
+      </a>
+    `;
+    // Agrega el elemento de lista al elemento channelList poner ${channelName} arroba si quiero que aparesca el nombre
+    channelList.appendChild(listItem);
+  }
+});
+
+// Define la función searchChannels
+function searchChannels() {
+  // Obtiene el valor del campo de búsqueda y lo convierte a minúsculas
+  const input = document.getElementById('channel-search').value.toLowerCase();
+  // Obtiene la lista de canales
+  const list = document.getElementById('channel-list');
+  // Obtiene los elementos de lista de la lista de canales
+  const items = list.getElementsByTagName('li');
+  // Convierte los elementos de lista en un array y itera sobre cada uno
+  Array.from(items).forEach(item => {
+    // Obtiene el nombre del canal del elemento de lista y lo convierte a minúsculas
+    const channelName = item.textContent.toLowerCase();
+    // Verifica si el nombre del canal incluye el valor de búsqueda
+    if (channelName.includes(input)) {
+      // Muestra el elemento de lista si el nombre del canal incluye el valor de búsqueda
+      item.style.display = 'flex';
+    } else {
+      // Oculta el elemento de lista si el nombre del canal no incluye el valor de búsqueda
+      item.style.display = 'none';
+    }
+  });
+}
+
+
